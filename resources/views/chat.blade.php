4cc7e0daf3d5a5977ecc645ca729d9d9cefe82fd
TommyRivera21 <68372501+TommyRivera21@users.noreply.github.com>
2022-11-28 03:46:27 -0500

    Final commit

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,7 +2,8 @@
 <html lang="en" >
 	<head>
 		<meta charset="UTF-8">
-		<title>CodePen - Simple Chat UI</title>
+		<title> Quotech | Chat Soporte</title>
+		<link rel="shortcut icon" href="/logo.png">
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
